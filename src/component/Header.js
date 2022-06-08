476a5167b7e342ba906e1f123680dfb6cdf5b644
codewithkevin <oseikelvin709@gmail.com>
2022-06-08 18:26:11 +0000

    updating header files

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,9 +4,14 @@ import React from "react";
 const Header = () => {
     return (
         <div className="navbar">
-            <h3>
+
+            <h3 className="logo">
                 LOGO
             </h3>
+
+            <h6 className="logo-text">MEME CREATOR BY KEVIN</h6>
+
+
         </div>
     )
 };
