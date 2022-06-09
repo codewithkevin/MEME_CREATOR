aa56cd0f21515ca507534c3e58af5ec3483ae8f5
codewithkevin <oseikelvin709@gmail.com>
2022-06-09 01:54:08 +0000

    update

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 
 const Meme = () => {
+    
+    function Say() {
+        alert('Hello')
+    }
+
     return (
         <main className="Meme">
 
             <from className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input  type="text" placeholder="Bottom text" className="form--input"/>
-                <button className="form--button">
+                <button onClick={Say} className="form--button">
                     Find A meme Image
                 </button>
             </from>
