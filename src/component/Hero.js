dc041a4eab13be089dfb86a4eb200ef331fdb787
codewithkevin <oseikelvin709@gmail.com>
2022-06-09 01:33:32 +0000

    update

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -6,8 +6,8 @@ const Meme = () => {
         <section className="Meme">
 
             <from className="form">
-                <input type="text" className="form--input"/>
-                <input  type="text" className="form--input"/>
+                <input type="text" placeholder="Top text" className="form--input"/>
+                <input  type="text" placeholder="Bottom text" className="form--input"/>
                 <button className="form--button">
                     Find A meme Image
                 </button>
