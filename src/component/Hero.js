48e46ec97881a50a24c1ce74a7e88ba01ff5dbf7
codewithkevin <oseikelvin709@gmail.com>
2022-06-09 20:13:28 +0000

    setting up meme data

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Meme = () => {
     
     function Say() {
-        alert('Hello')
+        alert('Hello, I got Clicked')
     }
 
     return (
@@ -13,9 +13,11 @@ const Meme = () => {
             <from className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input  type="text" placeholder="Bottom text" className="form--input"/>
+                
                 <button onClick={Say} className="form--button">
                     Find A meme Image
                 </button>
+            
             </from>
 
         </main>
