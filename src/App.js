e6e51e65c5c9469d48cb93731b4386676e817ea3
codewithkevin <oseikelvin709@gmail.com>
2022-06-18 04:26:58 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,66 @@ import './App.css';
 import Header from './component/Header'
 import Meme from './component/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
 
 function App() {
+  
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: null,
+});
 
+const [allMemeImages, setAllMemeImages] = React.useState([]);    
+
+React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(response => response.json())
+    .then(data => { setAllMemeImages(data.data.memes) })
+}, []);
+
+function Say() {
+    const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+    const url = allMemeImages[randomNumber].url;
+    setMeme(prevSate => {
+        return {
+            ...prevSate,
+            randomImage: url
+        }
+    })
+}
+
+
+
+function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme(prevState => {
+        return {
+            ...prevState,
+            [name]: value
+        }
+    })
+}
+
+function imageHandler(event) {
+    const reader = new FileReader();
+    reader.onload = () => { 
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                randomImage: reader.result
+            }
+        }
+        )
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+  const upper = meme.topText
+  const lower = meme.bottomText
+  const image = meme.randomImage
+  
 
   return (
     <div className="App">
@@ -14,7 +71,14 @@ function App() {
       </div>
 
       <div>
-        <Meme />
+        <Meme 
+          upper={upper}
+          lower={lower}
+          image={image}
+          Say={Say}
+          handleChange={handleChange}
+          imageHandler={imageHandler}
+        />
       </div>
 
     </div>
--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,65 +1,9 @@
 import React from 'react';
 import image from './images/japan.jpg';
 
-const Meme = () => {
-
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImage: image,
-    });
-
-    const [allMemeImages, setAllMemeImages] = React.useState([]);    
-    
-    
-    React.useEffect(() => {
-        async function getMemes() {
-            const res = await fetch("https://api.imgflip.com/get_memes")
-            const data = await res.json()
-            setAllMemeImages(data.data.memes)
-        }
-        getMemes()
-    }, [])
-    
-    function Say() {
-        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
-        const url = allMemeImages[randomNumber].url;
-        setMeme(prevSate => {
-            return {
-                ...prevSate,
-                randomImage: url
-            }
-        })
-    }
+const Meme = (props) => {
 
     
-
-    function handleChange(event) {
-        const { name, value } = event.target;
-        setMeme(prevState => {
-            return {
-                ...prevState,
-                [name]: value
-            }
-        })
-    }
-
-    function imageHandler(event) {
-        const reader = new FileReader();
-        reader.onload = () => { 
-            setMeme(prevState => {
-                return {
-                    ...prevState,
-                    randomImage: reader.result
-                }
-            }
-            )
-        }
-        reader.readAsDataURL(event.target.files[0]);
-    }
-
-    
-
     return (
         <main>
 
@@ -70,17 +14,9 @@ const Meme = () => {
                 placeholder="Top Text"
                 className="form--input"
                 name="topText"
-                value={meme.topText}
-                // onChange={(e) => {
-                //     setMeme(prevState => {
-                //         return {
-                //             ...prevState,
-                //             topText: e.target.value
-                //         }
-                //     }
-                // )}
-                // }
-                onChange = {handleChange}
+                value={props.upper}
+                
+                onChange = {props.handleChange}
                 />
                 
 
@@ -89,11 +25,11 @@ const Meme = () => {
                     placeholder="Bottom Text"
                     className="form--input"
                     name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
+                    value={props.lower}
+                    onChange={props.handleChange}
                 />
 
-                <button onClick={Say} className="form--button">
+                <button onClick={props.Say} className="form--button">
                                 Find A meme Image
                 </button>
                    
@@ -101,7 +37,7 @@ const Meme = () => {
                     type="file"
                     name="randomImage"
                     accept="image/*"
-                    onChange={imageHandler}
+                    onChange={props.imageHandler}
                     className="form--upload"
                 />
                         
@@ -109,9 +45,9 @@ const Meme = () => {
 
                 <div className="meme">
                     
-                    <img src={meme.randomImage} alt="meme" className="meme--image" />
-                    <h2 className="meme--text top">{meme.topText}</h2>
-                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                    <img src={props.image} alt="meme" className="meme--image" />
+                    <h2 className="meme--text top">{props.upper}</h2>
+                    <h2 className="meme--text bottom">{props.lower}</h2>
 
                 </div>
 
